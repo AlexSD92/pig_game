fb1de60f612bc07a329b41f7667765b73a467ef7
AlexSD92 <alex.spara.dominguez@gmail.com>
2024-03-02 09:04:52 +0000

    Create addScore function which takes the current score and adds dice roll

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-let diceImage = document.querySelector("#dice");
+const diceImage = document.querySelector("#dice");
+const playerBoard1TotalScore = document.querySelector(
+  "#player-board1-total-score"
+);
+const playerBoard2TotalScore = document.querySelector(
+  "#player-board2-total-score"
+);
+const playerBoard1RoundScore = document.querySelector(
+  "#player-board1-round-score"
+);
+const playerBoard2RoundScore = document.querySelector(
+  "#player-board2-round-score"
+);
+
+function addScore(score) {
+  currentScore = Number(playerBoard1RoundScore.textContent);
+  newScore = currentScore + score;
+  playerBoard1RoundScore.textContent = newScore;
+}
 
 function rollTheDice() {
   let dice = Math.floor(Math.random() * 6) + 1;
   console.log(dice);
   diceImage.src = `assets/dice${dice}.png`;
+  addScore(dice);
 }
